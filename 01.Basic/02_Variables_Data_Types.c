1b512d5ac2a4ccd05fcbd2c020984a3666cb0d2b
Alok dhar dwivedi <alokd9919911@gmail.com>
2026-09-06 22:34:08 +0530

    Create 02_Variables_Data_Types.c

---
+++ 01.Basic/02_Variables_Data_Types.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int age = 20;
+float marks = 85.3;
+char grade = 'A';
+printf("Age = %d\n",  age);
+printf("Marks = %f\n", marks);
+printf("Grade = %c\n", grade);
+return 0;
+}
