69dfdf6cff397af4af866a478ed0cff447f72ecd
Alok dhar dwivedi <alokd9919911@gmail.com>
2026-09-06 22:54:37 +0530

    Create 04_Operators.c

---
+++ 01.Basic/04_Operators.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main () {
+  int a = 12;
+  int b = 25;
+printf(" Addition = %d\n", a+b);
+printf(" Substraction = %d\n", a-b);
+printf(" Multiplication = %d\n", a*b);
+printf(" Division = %d\n", a/b );
+return 0;
+}
