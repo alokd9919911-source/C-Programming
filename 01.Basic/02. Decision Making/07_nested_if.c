da690255993417a94da46ed1ec8a23a6f634c6f8
Alok dhar dwivedi <alokd9919911@gmail.com>
2026-09-07 21:39:10 +0530

    Create 07_nested_if.c

---
+++ 01.Basic/02. Decision Making/07_nested_if.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+  int age;
+int marks;
+printf("Enter your age:");
+scanf("%d", &age);
+printf("Enter your marks:");
+scanf("%d", &marks);
+if(age>=18) {
+if(marks>=50) {
+print("Eligible");
+}
+}
+return 0;
+}
+  
