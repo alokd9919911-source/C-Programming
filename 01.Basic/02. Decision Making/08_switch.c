bfebe9139f9041242d728d21d3408852f9630e1b
Alok dhar dwivedi <alokd9919911@gmail.com>
2026-09-07 21:42:29 +0530

    Create 08_switch.c

---
+++ 01.Basic/02. Decision Making/08_switch.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+
+    printf("1. Tea\n");
+    printf("2. Coffee\n");
+    printf("3. Juice\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("You selected Tea");
+            break;
+
+        case 2:
+            printf("You selected Coffee");
+            break;
+
+        case 3:
+            printf("You selected Juice");
+            break;
+
+        default:
+            printf("Invalid choice");
+    }
+
+    return 0;
+}
