59f6b6d114e762bf74ed32c61e040ce496bccceb
Alok dhar dwivedi <alokd9919911@gmail.com>
2026-09-15 18:25:49 +0530

    Create 35_Dynamic_memory_allocation.c

---
+++ 06_Advanced/35_Dynamic_memory_allocation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *p;
+
+    p = (int *)malloc(sizeof(int));
+
+    *p = 50;
+
+    printf("Value = %d\n", *p);
+
+    free(p);
+
+    return 0;
+}
