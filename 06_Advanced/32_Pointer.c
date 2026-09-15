15e10d23ffa2905f4b980c7fd4eb468b4b8f33de
Alok dhar dwivedi <alokd9919911@gmail.com>
2026-09-15 18:20:31 +0530

    Create 32_Pointer.c

---
+++ 06_Advanced/32_Pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int *p = &a;
+
+    printf("Value of a = %d\n", a);
+    printf("Address of a = %p\n", (void*)&a);
+    printf("Value using pointer = %d\n", *p);
+
+    return 0;
+}
